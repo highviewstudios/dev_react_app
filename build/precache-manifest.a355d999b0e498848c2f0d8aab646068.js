68e79364cd228d7803f69246d044521ae4f9d60e
Shaun Evans <shaun.evans@high-view-studios.co.uk>
2020-06-15 17:20:44 +0100

    Inital commit for the react development project

---
+++ build/precache-manifest.a355d999b0e498848c2f0d8aab646068.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6298e223da28fbfadd0ffb9237837fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b23f325368912e4e3e4",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8a85b1fe3aeaab8748e8",
+    "url": "/static/js/2.0c12533d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0c12533d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b23f325368912e4e3e4",
+    "url": "/static/js/main.393ddd38.chunk.js"
+  },
+  {
+    "revision": "b9e4a70ad007d5d7972d",
+    "url": "/static/js/runtime-main.5a4c86da.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
